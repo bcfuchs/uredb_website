5fcbafe793e0fecfc7d6bdb16d6e374e8e27c31d
Brian <bcfuchs@gmail.com>
2016-11-16 22:00:42 +0000

    cleaning up

---
+++ web-app/js/global.js
@@ -0,0 +1,12 @@
+easterEgg('z',function(){
+  $(document).ready(function(){
+    $(".dev-features").toggle();
+  });
+  });
+
+easterEgg('v',function(){
+  $(document).ready(function(){
+    $("#versioninfo").toggle();
+  });
+  });
+    
