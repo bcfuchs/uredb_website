7d3777a9d03b4071e248af398648d2048a7f0254
Brian <bcfuchs@gmail.com>
2018-01-21 23:02:38 +0000

    OPEN - task 196: new client tests... http://github.com/bcfuchs/uredb_website/issues/issue/196

--- test/client/jasmine/spec/europeana_widget_Spec.js
+++ test/client/jasmine/spec/europeana_widget_Spec.js
@@ -1,16 +1,18 @@
 describe('europeana_widget tests',function(){
+  
   var pre_init = window.eu_widget_pre_init_funcs;
-  console.log(pre_init)
+  
+
     it('test get_whitelist_providers_facet',function(){
     
       var providers = ["ure museum","british museum"];
+      var expected = '&qf=provider_aggregation_edm_dataProvider:%22ure%20museum%22+OR+provider_aggregation_edm_dataProvider:%22british%20museum%22'
       var res = pre_init.get_whitelist_providers_facet(providers);
-      console.log(res)
-      fail(res);
+      expect(res).toEqual(expected);
     });
     
     it('testreset_querycache',function(){
-     expect(1).toEqual(1)
+     fail();
     
     });
 
