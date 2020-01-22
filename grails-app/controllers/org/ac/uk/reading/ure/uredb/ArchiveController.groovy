2f5327b33dd19c7f2063b822dd7375558abe5011
Brian <bcfuchs@gmail.com>
2020-01-22 07:54:04 +0000

    archive table + code

---
+++ grails-app/controllers/org/ac/uk/reading/ure/uredb/ArchiveController.groovy
@@ -0,0 +1,11 @@
+package org.ac.uk.reading.ure.uredb
+
+/**
+ * ArchiveController
+ * A controller class handles incoming web requests and performs actions such as redirects, rendering views and so on.
+ */
+class ArchiveController {
+
+	static scaffold = true
+//	def index = { }
+}
